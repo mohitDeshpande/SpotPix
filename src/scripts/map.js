c7d0646b83d17c051a20472624b62d0a57ddc87a
Mohit Deshpande <mohitdeshpande@icloud.com>
2017-03-14 09:32:45 -0400

    Create search functionality in the map

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -5,14 +5,85 @@
  * all methods and code related to the Google Maps JavaScript API goes here
  */
 
-var map;
 
-var toronto = {lat:43.6426, lng: -79.3871};
+map = null;
+var toronto = {lat: 43.6426, lng: -79.3871};
 
 function initMap() {
+
     map = new google.maps.Map(document.getElementById('map'), {
         center: toronto,
-        zoom: 12
+        zoom: 12,
+        mapTypeId: 'roadmap'
+    });
+
+    createMapSearch(map);
+
+}
+
+function createMapSearch(map) {
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('map-search');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
+
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function (marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
     });
 }
-// TODO add map search box https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
+
+
+
+
+
